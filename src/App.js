b3e154898ebb2df1ada899dd21e6823e6cae9a59
Daniel Miller <lontano.composer@gmail.com>
2019-05-07 16:18:39 -0400

    added background image and working API method

--- src/App.js
+++ src/App.js
@@ -1,28 +1,72 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
+if (typeof window !== 'undefined') {
+    window.React = React;
+}
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            apiKey: ''
+        };
+        this.updateKey = this.updateKey.bind(this);
+        this.getApi = this.getApi.bind(this);
+    }
+
+    updateKey(event) {
+        this.setState({
+            apiKey: event.target.value
+        });
+    }
+
+    getApi(e) {
+        const url = `https://www.n2yo.com/rest/v1/satellite/above/41.702/-76.014/0/70/18/&apiKey=${this.state.apiKey}`;
+        fetch(url)
+            .then(response => response.json())
+            .then(json => console.log(json))
+            .catch(err => console.log(err));
+        e.preventDefault();
+    }
+
+    render() {
+        console.log("TEST")
+        let output = null;
+        if (this.state.apiKey) {
+            output = <RenderInput input={this.state.apiKey} />
+        }
+        return (
+            <div className="App">
+                <header className="App-header">Eyes Above</header>
+                <EnterKey input={this.state.apiKey} handleChange={this.updateKey} getApi={this.getApi} />
+                {output}
+            </div>
+        );
+    }
+}
+
+class EnterKey extends Component {
+    render() {
+        return (
+            <div className="Input">
+                <form onSubmit={this.props.getApi}>
+                    <input type="text" value={this.props.input} onChange={this.props.handleChange} />
+                    <input type="submit" value="Submit" />
+                </form>
+            </div>
+        );
+    }
+}
+
+class RenderInput extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>Key Entered: {this.props.input}</p>
+            </div>
+        );
+    }
 }
 
 export default App;
